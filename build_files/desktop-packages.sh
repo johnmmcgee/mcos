d7854f4a2cbe6b881c018efee56cdb459ac39ee5
John McGee <jmcgee@kidraye.com>
2024-11-20 21:35:10 -0500

    didnt work

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -81,6 +81,3 @@ sed -i "s@Exec=zed@Exec=/usr/lib/zed.app/libexec/zed-editor@g" /usr/share/applic
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
 dnf5 install -y code
-
-# ansible-navigator
-python3 -m pip install ansible-navigator
