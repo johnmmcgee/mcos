57ba5e0c0d1413df168d442e20e2baef90e6e764
John McGee <jmcgee@kidraye.com>
2025-03-23 22:10:30 -0400

    remove 389 stuff for now

--- build_files/server-packages.sh
+++ build_files/server-packages.sh
@@ -7,7 +7,7 @@ echo "Running server packages scripts..."
 
 # common packages installed to servers
 dnf5 install -y \
-  389-ds-base \
+#  389-ds-base \
   acpica-tools \
   btop \
   cockpit-storaged \
