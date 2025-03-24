6fe66cea17a17c313f04d10cc4156dd6bcfb5c14
John McGee <jmcgee@kidraye.com>
2025-03-23 21:58:29 -0400

    try again

--- build_files/server-cockpit-modules.sh
+++ build_files/server-cockpit-modules.sh
@@ -29,7 +29,7 @@ echo "389 DS Console"
 dnf5 install -y nodejs
 git clone --quiet https://github.com/389ds/389-ds-base.git
 cd 389-ds-base/src/cockpit/389-console/
-npm install
+npm install > /dev/null
 ./build.js
 cd -
 cp -r 389-ds-base/src/cockpit/389-console/dist /usr/share/cockpit/389-console
