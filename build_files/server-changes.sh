e541a110116f8add8a7c1b37018e968a58c99693
John McGee <jmcgee@kidraye.com>
2025-03-29 20:24:57 -0400

    lets see if this works

--- build_files/server-changes.sh
+++ build_files/server-changes.sh
@@ -5,8 +5,5 @@ set -euox pipefail
 echo "Hacky stuff that shouldn't be here"
 
 if [[ ${IMAGE} =~ ucore ]]; then
-    dnf remove -y \
-        containerd moby-engine runc
-    rm -f /usr/bin/docker-compose
-    rm -fr /usr/libexec/docker
+    dnf download console-389-ds -y && rpm2cpio console-389-ds-*.rpm | cpio -idmv -D /
 fi
