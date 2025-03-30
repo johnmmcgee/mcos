0716aacccabfcbd970785b260ce193ea5fed6b48
John McGee <jmcgee@kidraye.com>
2025-03-29 20:35:46 -0400

    fix typo

--- build_files/server-changes.sh
+++ build_files/server-changes.sh
@@ -5,5 +5,5 @@ set -euox pipefail
 echo "Hacky stuff that shouldn't be here"
 
 if [[ ${IMAGE} =~ ucore ]]; then
-    dnf download console-389-ds -y && rpm2cpio console-389-ds-*.rpm | cpio -idmv -D /
+    dnf download cockpit-389-ds -y && rpm2cpio cockpit-389-ds-*.rpm | cpio -idmv -D /
 fi
