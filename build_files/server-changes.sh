d99ce4bf1fdf2219c212f30c480ee642112b4153
John McGee <jmcgee@kidraye.com>
2025-03-29 20:51:54 -0400

    change syntax a little

--- build_files/server-changes.sh
+++ build_files/server-changes.sh
@@ -5,5 +5,5 @@ set -euox pipefail
 echo "Hacky stuff that shouldn't be here"
 
 if [[ ${IMAGE} =~ ucore ]]; then
-    dnf download cockpit-389-ds -y && rpm2cpio cockpit-389-ds-*.rpm | cpio -idmv -D /
+    dnf download cockpit-389-ds -y && rpm2cpio cockpit-389-ds-*.rpm | cpio -idmv --directory=/
 fi
