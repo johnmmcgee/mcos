f0b41ea5047ae63b7e60de83e85d7e93db9e58bd
John McGee <jmcgee@kidraye.com>
2025-05-17 14:13:35 -0400

    remove zed for now

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -67,14 +67,14 @@ dnf remove -y \
 /ctx/build_files/github-release-install.sh twpayne/chezmoi x86_64
 
 # Zed because why not?
-curl -Lo /tmp/zed.tar.gz \
-    https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
-mkdir -p /usr/lib/zed.app/
-tar -xvf /tmp/zed.tar.gz -C /usr/lib/zed.app/ --strip-components=1
-ln -s /usr/lib/zed.app/bin/zed /usr/bin/zed
-cp /usr/lib/zed.app/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
-sed -i "s@Icon=zed@Icon=/usr/lib/zed.app/share/icons/hicolor/512x512/apps/zed.png@g" /usr/share/applications/dev.zed.Zed.desktop
-sed -i "s@Exec=zed@Exec=/usr/lib/zed.app/libexec/zed-editor@g" /usr/share/applications/dev.zed.Zed.desktop
+#curl -Lo /tmp/zed.tar.gz \
+#    https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
+#mkdir -p /usr/lib/zed.app/
+#tar -xvf /tmp/zed.tar.gz -C /usr/lib/zed.app/ --strip-components=1
+#ln -s /usr/lib/zed.app/bin/zed /usr/bin/zed
+#cp /usr/lib/zed.app/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
+#sed -i "s@Icon=zed@Icon=/usr/lib/zed.app/share/icons/hicolor/512x512/apps/zed.png@g" /usr/share/applications/dev.zed.Zed.desktop
+#sed -i "s@Exec=zed@Exec=/usr/lib/zed.app/libexec/zed-editor@g" /usr/share/applications/dev.zed.Zed.desktop
 
 # vscode stuff
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
