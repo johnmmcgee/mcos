361c29e790fcdeb454022b264fd29d2bb94058be
John McGee <jmcgee@kidraye.com>
2025-07-26 16:02:51 -0400

    chore(build): add Bitwarden polkit authentication configuration
    * Adds policy configuration for Bitwarden unlock action
    * Sets authentication requirements for Bitwarden on Linux desktop

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -98,6 +98,24 @@ curl -L https://bitwarden.com/download/?app=desktop\&platform=linux\&variant=rpm
 dnf install -y bitwarden.rpm
 mv /var/opt/Bitwarden /usr/share/factory/bitwarden
 echo "L  /opt/Bitwarden  -  -  -  -  /usr/share/factory/bitwarden" > /usr/lib/tmpfiles.d/microsoft.conf
+cat <<EOF > /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE policyconfig PUBLIC
+"-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN"
+"http://www.freedesktop.org/standards/PolicyKit/1.0/policyconfig.dtd">
+
+<policyconfig>
+    <action id="com.bitwarden.Bitwarden.unlock">
+      <description>Unlock Bitwarden</description>
+      <message>Authenticate to unlock Bitwarden</message>
+      <defaults>
+        <allow_any>no</allow_any>
+        <allow_inactive>no</allow_inactive>
+        <allow_active>auth_self</allow_active>
+      </defaults>
+    </action>
+</policyconfig>
+EOF
 
 # vscode stuff
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
