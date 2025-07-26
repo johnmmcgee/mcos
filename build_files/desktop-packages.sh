ec32e51847af2be4dcb1f428635cdbf060919cbb
John McGee <jmcgee@kidraye.com>
2025-07-26 14:29:05 -0400

    chore(build): fix konsave installation command

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -68,7 +68,7 @@ fi
 if [[ ${IMAGE} =~ aurora ]]; then
   dnf -y copr enable deltacopy/darkly
   dnf install -y darkly
-  pip install --prefix /usr --root-user-action konsave || true
+  pip install --prefix /usr --root-user-action  konsave || true
 fi
 
 # common packages excluded from desktop
