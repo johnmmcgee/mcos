547d083f188346c23b9128000ee7ddf771d3559a
John McGee <jmcgee@kidraye.com>
2026-02-26 13:04:13 -0500

    fix desktop link filename

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -93,7 +93,7 @@ curl -Lo /tmp/zed.tar.gz \
 mkdir -p /usr/lib/zed.app/
 tar -xvf /tmp/zed.tar.gz -C /usr/lib/zed.app/ --strip-components=1
 #ln -s /usr/lib/zed.app/bin/zed /usr/bin/zed
-cp /usr/lib/zed.app/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
+cp /usr/lib/zed.app/share/applications/dev.zed.Zed.desktop /usr/share/applications/dev.zed.Zed.desktop
 sed -i "s@Icon=zed@Icon=/usr/lib/zed.app/share/icons/hicolor/512x512/apps/zed.png@g" /usr/share/applications/dev.zed.Zed.desktop
 sed -i "s@Exec=zed@Exec=/usr/lib/zed.app/libexec/zed-editor@g" /usr/share/applications/dev.zed.Zed.desktop
 
