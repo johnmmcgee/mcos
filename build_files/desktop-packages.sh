4897e642cc9e01902ed0486c3c1c924022738768
John McGee <jmcgee@kidraye.com>
2026-05-01 00:13:00 -0400

    Add RPMFusion repo URLs to dnf install command

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -80,7 +80,7 @@ if [[ ${IMAGE} =~ aurora ]]; then
 fi
 
 # common packages excluded from desktop
-dnf install -y 
+dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf config-manager setopt fedora-cisco-openh264.enabled=1
