4d3172b185026531082cd9a62bb1b2cdf8d667bd
John McGee <jmcgee@kidraye.com>
2026-05-01 00:52:01 -0400

    Update desktop-packages.sh

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -81,7 +81,7 @@ fi
 
 # common packages excluded from desktop
 dnf install -y \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf config-manager setopt fedora-cisco-openh264.enabled=1
 dnf swap ffmpeg-free ffmpeg --allowerasing
